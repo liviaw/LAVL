1976b45d6296c68f70b2aa52097dc2ae650489c5
Vivian Shen <vivianshen046110@gmail.com>
2020-07-19 00:23:41 +1000

    map things

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './screens/home';
 import MapScreen from './screens/map';
@@ -10,41 +11,49 @@ import SettingsScreen from './screens/settings';
 import LogIn from './screens/LogIn';
 import SelectAvatar from './screens/SelectAvatar';
 import GameScreen from './screens/game';
+import LoadingScreen from './screens/loading';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   const [sign, setSign] = useState(1);
-  function SignedIn () {
+  function SignedIn() {
     setSign(2);
   }
 
-  function HomePage () {
+  function HomePage() {
     setSign(4);
   }
-  
+
   if (sign == 4) {
     return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Map" component={MapScreen} />
-          <Tab.Screen name="Chat" component={ChatScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-          <Tab.Screen name="Game" component={GameScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    )
-  } else if (sign == 2) {
-    return (
-      <SelectAvatar signedIn={HomePage}/>
-    )
-  } else {
-    return (<LogIn signedIn={SignedIn} />)
-  };
+      <React.Fragment>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Chat" component={ChatScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+            <Tab.Screen name="Game" component={GameScreen} />
+          </Tab.Navigator>
+          {/* <Stack.Navigator>
+          <Stack.Screen name="Loading" component={LoadingScreen} />
+        </Stack.Navigator> */}
+        </NavigationContainer>
 
-   
-    
+        {/* <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Loading" component={LoadingScreen} />
+          </Stack.Navigator>
+        </NavigationContainer> */}
+      </React.Fragment>
+    );
+  } else if (sign == 2) {
+    return <SelectAvatar signedIn={HomePage} />;
+  } else {
+    return <LogIn signedIn={SignedIn} />;
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/map.js
+++ screens/map.js
@@ -5,30 +5,73 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function MapScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image source={require('../assets/test.jpg')} />;
-      <Text>What would you like to do?</Text>
-      <Button
-        onPress={() => {
-          //   <Image source={require('../assets/test.jpg')} />;
-          <Image
-            style={styles.tinyLogo}
-            source={require('../assets/jack-russel-looping.gif')}
-          />;
-          console.log('Wef');
-          navigation.navigate('Chat');
-        }}
-        title="Chat"
-      />
-      <Button
-        onPress={() => {
-          navigation.navigate('Game');
-        }}
-        title="Game"
-      />
-    </View>
+    <React.Fragment>
+      <View style={styles.map}>
+        <Text>What adventure do you want to go on?</Text>
+        <View style={styles.controlSpace}>
+          <View style={styles.buttonView}>
+            <Button
+              onPress={() => {
+                navigation.navigate('Chat');
+              }}
+              title="Chat"
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button
+              onPress={() => {
+                navigation.navigate('Game');
+              }}
+              title="Game"
+            />
+          </View>
+        </View>
+      </View>
+
+      {/* Travelling image - to be on loading screen */}
+      <View>
+        <Image source={require('../assets/walk.gif')} />
+      </View>
+
+      {/* Two pets talking to each other */}
+      {/* <View>
+        <Image source={require('../assets/test.png')} />
+      </View> */}
+    </React.Fragment>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // position: relative;
+    // backgroundColor: '#fff';
+    // borderWidth: 40;
+    // padding: 24;
+    // text-align: center;
+    // border-left: 24 solid #fff;
+    // border-right: 12px solid transparent;
+    // border-top: 12px solid #fff;
+    // border-bottom: 20px solid transparent;
+    // left: 32px;
+    // bottom: -24px;
+  },
+  controlSpace: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    // backgroundColor: '#8E7EFB',
+  },
+  buttonView: {
+    width: '30%',
+    padding: 10,
+    borderWidth: 4,
+    borderColor: 'white',
+    borderRadius: 6,
+    backgroundColor: '#8E7EFB',
+  },
+});
 
 // performTimeConsumingTask = async () => {
 //   return new Promise((resolve) =>
