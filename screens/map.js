5cb0e1298686d1af6b7334388e96f982762ae236
Vivian Shen <vivianshen046110@gmail.com>
2020-07-18 20:30:11 +1000

    image, pulling from liz

--- screens/map.js
+++ screens/map.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function MapScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image source={require('../assets/test.jpg')} />;
       <Text>What would you like to do?</Text>
       <Button
         onPress={() => {
+          //   <Image source={require('../assets/test.jpg')} />;
+          <Image
+            style={styles.tinyLogo}
+            source={require('../assets/jack-russel-looping.gif')}
+          />;
+          console.log('Wef');
           navigation.navigate('Chat');
         }}
         title="Chat"
       />
-
       <Button
         onPress={() => {
           navigation.navigate('Game');
@@ -23,5 +29,13 @@ function MapScreen({ navigation }) {
     </View>
   );
 }
+
+// performTimeConsumingTask = async () => {
+//   return new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve('result');
+//     }, 3000)
+//   );
+// };
 
 export default MapScreen;
