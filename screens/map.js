aa0e48afc601976ef0c379e956691c1ee0594051
Livia Wijayanti <liviawijayanti@gmail.com>
2020-07-19 07:30:01 +1000

    animals

--- screens/map.js
+++ screens/map.js
@@ -1,67 +1,330 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Overlay, OverlayComponent, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-function MapScreen({ navigation }) {
-  return (
-    <View>
-      <Image
-        source={require('../assets/walking.gif')}
-        style={styles.backgroundImage}
+
+import MapView, { UrlTile }  from 'react-native-maps';
+import ModalTester from './modal';
+
+
+export default class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false,
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <MapView
+        style={styles.map}
+        
+        initialRegion={{
+          latitude: -33.909990,
+          longitude: 151.222970,
+          latitudeDelta: 0.0982,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* <View style={styles.modal}>
+          <Overlay isVisible={this.state.isVisible} >
+          <Text>hi!</Text>
+          </Overlay>
+        </View> */}
+        <ModalTester/>
+
+        <MapView.Marker draggable
+          coordinate={{
+            latitude: -33.9173,
+            longitude: 151.2313}}
+          onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
+        >
+        <Image
+        style={styles.gifLogo}
+        source={require('../assets/jack-russel-looping.gif')}
       />
+      </MapView.Marker>
+
+
+
+      <MapView.Marker
+        coordinate={{
+          latitude: -33.849300,
+          longitude: 151.182440,
+        }}
+        title="Sydey Opera House"
+        description="dog"> 
+          <Image
+        style={styles.gifLogo}
+        source={require('../assets/doggo.gif')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.849300,
+          longitude: 151.182440,
+        }}> 
+          <Image
+        style={styles.gifLogo}
+        source={require('../assets/doggo.gif')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.849300,
+          longitude: 150.182440,
+        }}> 
+          <Image
+        style={styles.gifLogo}
+        source={require('../assets/doggo.gif')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -32.849300,
+          longitude: 155.182440,
+        }}> 
+          <Image
+        style={styles.gifLogo}
+        source={require('../assets/doggo.gif')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.8914,
+          longitude: 151.2497,
+        }}> 
+          <Image
+        style={styles.gifLogo}
+        source={require('../assets/doggo.gif')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        //accenture tower
+        coordinate={{
+          latitude: -33.849300,
+          longitude: 151.192440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./monkey.png')}
+      />
+        </MapView.Marker>
+        <MapView.Marker
+        //randwick
+        coordinate={{
+          latitude: -33.9132,
+          longitude: 151.192440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./monkey.png')}
+      />
+        </MapView.Marker>
+        <MapView.Marker
+        //randwick
+        coordinate={{
+          latitude: -33.9132,
+          longitude: 151.2431,
+        }}
+        title="Sydey Opera House"
+        description="dog"
+        > 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./kitten.png')}
+        onPress={() => {
+          this.setState({ isVisible: true })
+        }}
+      
+      />
+        </MapView.Marker>
+
+        <MapView.Marker isVisible={this.state.isVisible}
+        //randwick
+        coordinate={{
+          latitude: -33.9142,
+          longitude: 151.0931,
+        }}> 
+
+        <View  style={styles.mapViv} >
+        <Text>What adventure do you want to go on?</Text>
+        <View style={styles.controlSpace}>
+          <View style={styles.buttonView}>
+            <Button
+              onPress={() => {
+                navigation.navigate('Chat');
+              }}
+              title="Chat"
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button
+              onPress={() => {
+                navigation.navigate('Game');
+              }}
+              title="Game"
+            />
+          </View>
+        </View>
+      </View>
+
+
+        </MapView.Marker>
+
+
+        <MapView.Marker
+        //randwick
+        coordinate={{
+          latitude: -33.9152,
+          longitude: 151.2420,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./dog.png')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.949300,
+          longitude: 151.182440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./dog.png')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.9132,
+          longitude: 151.242,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./dog.png')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.909990,
+          longitude: 151.222970,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./dog.png')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.949300,
+          longitude: 151.152440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('../assets/cat.gif')}
+      />
+        </MapView.Marker>
+
+
+
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.849300,
+          longitude: 151.152440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('../assets/cat.gif')}
+      />
+        </MapView.Marker>
+
+        <MapView.Marker
+        coordinate={{
+          latitude: -35.849300,
+          longitude: 150.152440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('./monkey.png')}
+      />
+        </MapView.Marker>
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.849300,
+          longitude: 151.152840,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('../assets/dog.png')}
+      />
+        </MapView.Marker>
+        <MapView.Marker
+        coordinate={{
+          latitude: -33.949300,
+          longitude: 151.002440,
+        }}> 
+          <Image
+        style={styles.tinyLogo}
+        source={require('../assets/cat.gif')}
+      />
+        </MapView.Marker>
+        </MapView>
+
+
+
+      
     </View>
+    );
+  }
 
-    // <React.Fragment>
-    //   <View style={styles.map}>
-    //     <Text>What adventure do you want to go on?</Text>
-    //     <View style={styles.controlSpace}>
-    //       <View style={styles.buttonView}>
-
-    //         <Button
-    //           color="#fff"
-    //           onPress={() => {
-    //             navigation.navigate('Chat');
-    //           }}
-    //           title="Chat"
-    //         />
-    //       </View>
-    //       <View style={styles.buttonView}>
-    //         <Button
-    //           color="#fff"
-    //           onPress={() => {
-    //             navigation.navigate('Game');
-    //           }}
-    //           title="Game"
-    //         />
-    //       </View>
-    //     </View>
-    //   </View>
-
-    //   {/* Travelling image - to be on loading screen */}
-    //   <View>
-    //     <Image source={require('../assets/cat-walk.gif')} />
-    //   </View>
-
-    //   {/* Two pets talking to each other */}
-    //   {/* <View>
-    //     <Image source={require('../assets/test.png')} />
-    //   </View> */}
-    // </React.Fragment>
-  );
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    // flex: 1,
-    resizeMode: 'stretch',
-    width: 410,
-    height: 700,
-  },
-  map: {
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fcfafc',
+    backgroundColor: '#F5FCFF',
+
+  },
+  overlay: {
+    position: 'absolute'
+  },
+  map: {
+    left: 0,
+    right:0,
+    top:0,
+    bottom: 0,
+    position: 'absolute',
+
+  },
+  gifLogo: {
+    width:100,
+    height:100,
+  },
+  tinyLogo: {
+    width:50,
+    height:50,
+    
+  },
+
+  mapViv: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center', 
     // position: relative;
     // backgroundColor: '#fff';
     // borderWidth: 40;
@@ -77,7 +340,7 @@ const styles = StyleSheet.create({
   controlSpace: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    // backgroundColor: '#8E7EFB',
+    backgroundColor: 'green',
   },
   buttonView: {
     width: '30%',
@@ -87,14 +350,6 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     backgroundColor: '#c0d8ae',
   },
-});
 
-// performTimeConsumingTask = async () => {
-//   return new Promise((resolve) =>
-//     setTimeout(() => {
-//       resolve('result');
-//     }, 3000)
-//   );
-// };
+})
 
-export default MapScreen;
