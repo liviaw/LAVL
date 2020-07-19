b2bba10a4e5a4dc95d6383428b0ee500b8895e6b
Vivian Shen <vivianshen046110@gmail.com>
2020-07-19 14:28:57 +1000

    chat

--- screens/chat.js
+++ screens/chat.js
@@ -1,14 +1,27 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function ChatScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Chat!</Text>
+      {/* <Text>Chat!</Text> */}
+      <Image style={styles.container} source={require('../assets/chat.gif')} />
     </View>
   );
 }
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 410,
+    height: 900,
+    // flex: 1,
+    resizeMode: 'stretch',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // marginTop: 10,
+  },
+});
